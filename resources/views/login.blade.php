c3bfb544db0c897be432f3358af51a085c25e8f4
farisbud <burhannuddinm@gmail.com>
2022-10-08 14:44:10 +0700

    memperbaiki tampilan form login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,54 +1,61 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="{{ asset('asset/css/bootstrap.css') }}" rel="stylesheet" />
 </head>
 <body>
     <main>
-        <div class="d-flex justify-content-center pt-5">
-            <div class="card" style="width: 18rem;">
-                @if (session('pesan'))
-                <div class="alert alert-success">
-                    {{session('pesan')}}
-                  </div>
-                @endif
-                <div class="card-body" style="height: vh;">
-                <h5 class="card-title">Login</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Login User</h6>
-                <form action="" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="disabledTextInput" class="form-label">Email</label>
-                        <input type="email" id="disabledTextInput" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" placeholder="masukan email">
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
+        <div class="container">
+            <div class="d-flex align-items-center" style="height: 85vh">
+                <div class="col-md-10">
+                    <div class="row justify-content-center">
+                            <div class="card" style="width: 35rem;">
+                                @if (session('pesan'))
+                                <div class="alert alert-success">
+                                    {{session('pesan')}}
+                                </div>
+                                @endif
+                                <div class="card-body">
+                                <h5 class="card-title"><center>Login</center></h5>
+                                <h6 class="card-subtitle mb-4 text-muted"><center>SPK TOPSIS</center></h6>
+                                <form action="" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="mb-3 row">
+                                        <label for="staticEmail" class="col-sm-3 col-form-label">Username</label>
+                                        <div class="col-sm-8">
+                                          <input type="text" class="form-control @error('username') is-invalid @enderror" name="username">
+                                            @error('username')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                      </div>
+                                      <div class="mb-3 row">
+                                        <label for="inputPassword" class="col-sm-3 col-form-label">Password</label>
+                                        <div class="col-sm-8">
+                                          <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                      </div>
+                                      <div class="d-flex justify-content-center">
+                                        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                                      </div>
+                                </form>
+
+                                </div>
+                            </div>
                     </div>
-
-                    <div class="mb-3">
-                        <label for="disabledTextInput" class="form-label">password</label>
-                        <input type="password" id="disabledTextInput" name="password" class="form-control @error('password') is-invalid @enderror">
-                        @error('password')
-
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-
-
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
-
-                </form>
-
                 </div>
             </div>
         </div>
     </main>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="{{asset('asset/js/bootstrap.bundle.js')}}"></script>
 
 </body>
 </html>
